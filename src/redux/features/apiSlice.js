5749cef500ef0fcc168299365dea34e6b587a4fe
Abdul Mannan <muhammadabdulmannan21@gmail.com>
2025-08-02 17:27:59 +0600

    redux setup

---
+++ src/redux/features/apiSlice.js
@@ -0,0 +1,16 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+export const apiSlice = createApi({
+    reducerPath: 'api',
+    baseQuery: fetchBaseQuery({ baseUrl: 'https://jsonplaceholder.typicode.com/' }),
+    endpoints: (builder) => ({
+        getPosts: builder.query({
+            query: () => 'posts',
+        }),
+        getPostById: builder.query({
+            query: (id) => `posts/${id}`,
+        }),
+    }),
+})
+
+export const { useGetPostsQuery, useGetPostByIdQuery } = apiSlice
